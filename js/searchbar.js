0172e8cfe20bdffbd517baeb1d327e6e78ab459e
Tina <tina.bekkholt@hotmail.com>
2023-03-16 10:35:44 +0100

    testing products js

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,47 +1,50 @@
 const films = [
-    {name: `the addams family`},
-    {name: `batman`},
-    {name: `godzilla king of the monsters`},
-    {name: `hobbs & shaw`},
-    {name: `once upon a time in hollywood`},
-    {name: `scream`},
-    {name: `sweetheart`},
-    {name: `toy story 4`},
+    {title: `the addams family`,
+    posterSrc: `images/Square_Eyes_Cover6.jpeg`,
+    description: `The eccentrically macabre family moves to a bland surburb where Wednesday Addams friendship with the daughter of a hostile and conformist local reality show host exacerbates conflict between the families.`,
+    directedBy: `Directed by: Greg Tiernan, Conrad Vernon`,
+    featuring: `Featuring: Oscar Isaac, Charlize Theron, Chloë Grace Moretz, Finn Wolfhard and more.`,
+    },
+
+    {title: `batman`,
+    posterSrc: `/images/Square_Eyes_Cover7.jpeg`,
+    description: ``,
+    directedBy: ``,
+    featuring: ``,},
+
+    {title: `godzilla king of the monsters`,
+    posterSrc: `/images/Square_Eyes_Cover2.jpeg`,
+    description: ``,
+    directedBy: ``,
+    featuring: ``,},
+
+    {title: `hobbs & shaw`,
+    posterSrc: `/images/Square_Eyes_Cover1.jpeg`,
+    description: ``,
+    directedBy: ``,
+    featuring: ``,},
+
+    {title: `once upon a time in hollywood`,
+    posterSrc: `/images/Square_Eyes_Cover3.jpeg`,
+    description: ``,
+    directedBy: ``,
+    featuring: ``,},
+
+    {title: `scream`,
+    posterSrc: `/images/Square_Eyes_Cover4.jpeg`,
+    description: ``,
+    directedBy: ``,
+    featuring: ``,},
+
+    {title: `sweetheart`,
+    posterSrc: `/images/Square_Eyes_Cover5.jpeg`,
+    description: ``,
+    directedBy: ``,
+    featuring: ``,},
+
+    {title: `toy story 4`,
+    posterSrc: `/images/Square_Eyes_Cover8.jpeg`,
+    description: ``,
+    directedBy: ``,
+    featuring: ``,},
 ]
-
-// const searchBar = document.querySelector(`.input`);
-
-// searchBar.addEventListener("input" (e)) 
-//     let value = e.target.value
-
-//     if (value && value.trim().length > 0)
-//         value = value.trim().toLowerCase()
-//         setList(films.filter(name => {
-//             return person.name.includes(value)
-//         }))
-
-//         function setList(results){
-
-//             for (const name of results){
-//                 // creating a li element for each result item
-//                 const resultItem = document.createElement('li')
-        
-//                 // adding a class to each item of the results
-//                 resultItem.classList.add('result-item')
-        
-//                 // grabbing the name of the current point of the loop and adding the name as the list item's text
-//                 const text = document.createTextNode(films.name)
-        
-//                 // appending the text to the result item
-//                 resultItem.appendChild(text)
-        
-//                 // appending the result item to the list
-//                 list.appendChild(resultItem)
-//             }
-//         }
-
-//         const clearButton = document.getElementById('clear')
-
-// search.addEventListener("click", () => {
-//     window.location.href = "/product-specific.html"
-// })
