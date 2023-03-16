1fee0cfb1804788fc5e59afd6983879591f7047e
Tina <tina.bekkholt@hotmail.com>
2023-03-16 09:16:54 +0100

    first try searchbar

---
+++ js/searchbar.js
@@ -0,0 +1,47 @@
+const films = [
+    {name: `the addams family`},
+    {name: `batman`},
+    {name: `godzilla king of the monsters`},
+    {name: `hobbs & shaw`},
+    {name: `once upon a time in hollywood`},
+    {name: `scream`},
+    {name: `sweetheart`},
+    {name: `toy story 4`},
+]
+
+// const searchBar = document.querySelector(`.input`);
+
+// searchBar.addEventListener("input" (e)) 
+//     let value = e.target.value
+
+//     if (value && value.trim().length > 0)
+//         value = value.trim().toLowerCase()
+//         setList(films.filter(name => {
+//             return person.name.includes(value)
+//         }))
+
+//         function setList(results){
+
+//             for (const name of results){
+//                 // creating a li element for each result item
+//                 const resultItem = document.createElement('li')
+        
+//                 // adding a class to each item of the results
+//                 resultItem.classList.add('result-item')
+        
+//                 // grabbing the name of the current point of the loop and adding the name as the list item's text
+//                 const text = document.createTextNode(films.name)
+        
+//                 // appending the text to the result item
+//                 resultItem.appendChild(text)
+        
+//                 // appending the result item to the list
+//                 list.appendChild(resultItem)
+//             }
+//         }
+
+//         const clearButton = document.getElementById('clear')
+
+// search.addEventListener("click", () => {
+//     window.location.href = "/product-specific.html"
+// })
