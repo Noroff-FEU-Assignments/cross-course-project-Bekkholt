c5abec24cbc43ee4b3f1dab7e8d55bc1c341b730
Tina <tina.bekkholt@hotmail.com>
2023-03-24 14:41:14 +0100

    delete commented out

--- js/genre.js
+++ js/genre.js
@@ -13,22 +13,3 @@ for(let i = 0; i < indexes.length; i++){
         }
     }
 }
-
- // <div class="genre">
-  //   <div class="products">
-  //     <a href="product-specific.html?id=${film.id}"><img class="addams" src="images/Square_Eyes_Cover6.jpeg" alt="${film.alt}"></a>
-  //     <a href="checkout.html?id=${film.id}" class="cta price">$3.99</a>
-  //   </div>
-  //   <div class="products">
-  //     <a href="product-specific.html?id=${film.id}"><img class="batman" src="images/Square_Eyes_Cover7.jpeg" alt="${film.alt}"></a>
-  //     <a href="checkout.html?id=${film.id}" class="cta price">$3.99</a>
-  //   </div>
-  //   <div class="products">
-  //     <a href="product-specific.html?id=${film.id}"><img class="godzilla hide_mini" src="images/Square_Eyes_Cover2.jpeg" alt="${film.alt}"></a>
-  //     <a href="checkout.html?id=${film.id}" class="cta hide_mini">$3.99</a>
-  //   </div>
-  //   <div class="products">
-  //     <a href="product-specific.html?id=${film.id}"><img class="hobbs hide_small" src="images/Square_Eyes_Cover1.jpeg" alt="${film.alt}"></a>
-  //     <a href="checkout.html?id=${film.id}" class="cta hide_price">$3.99</a>
-  //   </div>
-  // </div>
